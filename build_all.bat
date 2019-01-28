31da030b4416cbd1aaad67ad6e6599cfb1970ab1
Sawyer McLane <samclane@gmail.com>
2019-01-27 17:06:34 -0700

    Updated build_all to optimize. Tag this for real.

--- build_all.bat
+++ build_all.bat
@@ -1,1 +1,1 @@
-pyinstaller --onefile main.spec & pyinstaller --onefile demo.spec & pyinstaller --onefile debug.spec
+set PYTHONOPTIMIZE=1 && pyinstaller --onefile main.spec & pyinstaller --onefile demo.spec & pyinstaller --onefile debug.spec
