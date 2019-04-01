c1f2a42f0f9f180760d97a21ac453f45c52ba832
Sawyer McLane <samclane@gmail.com>
2019-04-01 15:15:33 -0600

    linted splashscreen

--- ui/splashscreen.py
+++ ui/splashscreen.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Splash-screen class
+
+Displays LIFX-Control-Panel's icon while GUI loads
+"""
 from tkinter import Toplevel, Canvas, PhotoImage
 
 
@@ -7,6 +12,10 @@ class Splash:
     def __init__(self, root, file):
         self.__root = root
         self.__file = file
+        # Save the variables for later cleanup.
+        self.__window = None
+        self.__canvas = None
+        self.__splash = None
 
     def __enter__(self):
         # Hide the root while it is built.
@@ -16,22 +25,22 @@ class Splash:
         canvas = Canvas(window)
         splash = PhotoImage(master=window, file=self.__file)
         # Get the screen's width and height.
-        scrW = window.winfo_screenwidth()
-        scrH = window.winfo_screenheight()
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
         # Get the images's width and height.
-        imgW = splash.width()
-        imgH = splash.height()
+        img_width = splash.width()
+        img_height = splash.height()
         # Compute positioning for splash screen.
-        Xpos = (scrW - imgW) // 2
-        Ypos = (scrH - imgH) // 2
+        xpos = (screen_width - img_width) // 2
+        ypos = (screen_height - img_height) // 2
         # Configure the window showing the logo.
         window.overrideredirect(True)
-        window.geometry('+{}+{}'.format(Xpos, Ypos))
+        window.geometry('+{}+{}'.format(xpos, ypos))
         # Setup canvas on which image is drawn.
-        canvas.configure(width=imgW, height=imgH, highlightthickness=0)
+        canvas.configure(width=img_width, height=img_height, highlightthickness=0)
         canvas.grid()
         # Show the splash screen on the monitor.
-        canvas.create_image(imgW // 2, imgH // 2, image=splash)
+        canvas.create_image(img_width // 2, img_height // 2, image=splash)
         window.update()
         # Save the variables for later cleanup.
         self.__window = window
