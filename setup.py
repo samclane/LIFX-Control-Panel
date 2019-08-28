3b6a2a9e851bc6220b6efe22d1bde7a0c0bbbfa4
Sawyer McLane <samclane@gmail.com>
2019-08-28 15:48:14 -0600

    Updated project structure for PyPI/pip installation and deployment.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lifx_control_panel',
-      version='1.7.3',
+      version='1.7.4',
       description='An open source application for controlling your LIFX brand lights',
       url='http://github.com/samclane/LIFX-Control-Panel',
       author='Sawyer McLane',
@@ -10,4 +10,16 @@ setup(name='lifx_control_panel',
       packages=find_packages(),
       zip_safe=False,
       scripts=['lifx_control_panel/__main__.pyw'],
-      include_package_data=True)
+      include_package_data=True,
+      keywords=['lifx', 'iot', 'smartbulb', 'smartlight', 'lan', 'application'],
+      install_requires=[
+            'PyHook3',
+            'desktopmagic',
+            'win32gui',
+            'pyaudio',
+            'Pillow',
+            'lifxlan',
+            'pywin32',
+            'numexpr',
+            'numpy'
+      ])
