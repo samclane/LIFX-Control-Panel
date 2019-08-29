0416000d3688b43c056478e00b53ed99ed363f56
Sawyer McLane <samclane@gmail.com>
2019-08-29 02:05:10 -0600

    Added long-description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f
+
 setup(name='lifx_control_panel',
       version='1.7.4',
       description='An open source application for controlling your LIFX brand lights',
@@ -13,14 +16,16 @@ setup(name='lifx_control_panel',
       include_package_data=True,
       keywords=['lifx', 'iot', 'smartbulb', 'smartlight', 'lan', 'application'],
       install_requires=[
-            'PyHook3',
-            'desktopmagic',
-            'win32gui',
-            'pyaudio',
-            'Pillow',
-            'lifxlan',
-            'pywin32',
-            'numexpr',
-            'numpy'
+          'PyHook3',
+          'desktopmagic',
+          'win32gui',
+          'pyaudio',
+          'Pillow',
+          'lifxlan',
+          'pywin32',
+          'numexpr',
+          'numpy'
       ],
-      download_url="https://github.com/samclane/LIFX-Control-Panel/archive/1.7.4.tar.gz")
+      download_url="https://github.com/samclane/LIFX-Control-Panel/archive/1.7.4.tar.gz",
+      long_description=long_description,
+      long_description_content_type='text/markdown')
