3ebdf1428a5abdc03f5b13b43adf2687080db670
Sawyer McLane <samclane@gmail.com>
2020-08-08 15:24:54 -0600

    Black Formatted setup.py. Added 'classifiers' to setup, noting Python version >= 3.8

--- setup.py
+++ setup.py
@@ -2,33 +2,56 @@ from setuptools import setup, find_packages
 
 from lifx_control_panel._constants import VERSION
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name='lifx_control_panel',
-      version=str(VERSION),
-      description='An open source application for controlling your LIFX brand lights',
-      url='http://github.com/samclane/LIFX-Control-Panel',
-      author='Sawyer McLane',
-      author_email='samclane@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False,
-      scripts=['lifx_control_panel/__main__.pyw'],
-      include_package_data=True,
-      keywords=['lifx', 'iot', 'smartbulb', 'smartlight', 'lan', 'application'],
-      install_requires=[
-          'PyHook3',
-          'desktopmagic',
-          'win32gui',
-          'pyaudio',
-          'Pillow',
-          'lifxlan',
-          'pywin32',
-          'numexpr',
-          'numpy'
-      ],
-      download_url=f"https://github.com/samclane/LIFX-Control-Panel/archive/" + str(VERSION) + ".tar.gz",
-      long_description_content_type="text/markdown",
-      long_description=long_description,
-      )
+setup(
+    name="lifx_control_panel",
+    version=str(VERSION),
+    description="An open source application for controlling your LIFX brand lights",
+    url="http://github.com/samclane/LIFX-Control-Panel",
+    author="Sawyer McLane",
+    author_email="samclane@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    scripts=["lifx_control_panel/__main__.pyw"],
+    include_package_data=True,
+    keywords=["lifx", "iot", "smartbulb", "smartlight", "lan", "application"],
+    install_requires=[
+        "PyHook3",
+        "desktopmagic",
+        "win32gui",
+        "pyaudio",
+        "Pillow",
+        "lifxlan",
+        "pywin32",
+        "numexpr",
+        "numpy",
+    ],
+    download_url=f"https://github.com/samclane/LIFX-Control-Panel/archive/"
+    + str(VERSION)
+    + ".tar.gz",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 5 - Production",
+        # Indicate who your project is intended for
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 8.1",
+        "Operating System :: Microsoft :: Windows :: Windows 8",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Topic :: Home Automation",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.8",
+    ],
+)
